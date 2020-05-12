7f2a0e30dd7b8a79a839a5920507d029c59cefeb
nkhakwan <post_khan@yahoo.com>
2020-05-12 14:52:47 -0700

    committing after writing the function for calculating number of days

--- ToDoList/Models/WeekDay.cs
+++ ToDoList/Models/WeekDay.cs
@@ -1,17 +1,53 @@
 namespace DayOfTheWeekCalculator
 {
-  public class WeekDay
+  public class WeekDay : IDayCalculator
   {
     int Date {get; set;}
     int Year {get; set;}
 
-    int Month {get; set}
+    int Month {get; set;}
 
     public WeekDay (int date, int year, int month)
     {
       Date = date;
       Year = year;
       Month = month;
+    }
+
+    
+
+    string FirstDayOfTheYearCalculator()
+    {
+
+
+    }
+    int NumberOfDays()
+    {
+      int[] Months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+      int TotalDays = 0;
+      if (Month <2 && (Year%4) != 0)
+      {
+        for (int i = 0; i<Month; i++)
+        {
+          TotalDays += Months[i];
+        } 
+      }
+      else 
+      {
+        for (int i = 0; i<Month; i++)
+        {
+          TotalDays += Months[i];
+        } 
+        TotalDays = TotalDays +1;
+      }
+      TotalDays = TotalDays + Date;
+      return TotalDays;
+    }
+
+    string DayFinder()
+    {
+
+
     }
 
 
