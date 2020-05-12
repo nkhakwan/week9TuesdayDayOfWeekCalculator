32c22a7056adfec6cea856f228e8daa85f37edd5
nkhakwan <post_khan@yahoo.com>
2020-05-12 11:19:48 -0700

    committing updated files with code additions that didn't work for now

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -20,7 +20,7 @@ namespace ToDoList.Tests
       Item newItem = new Item("test");
       Assert.AreEqual(typeof(Item), newItem.GetType());
       //Console.WriteLine(typeof(Item));
-      //Console.WriteLine(newItem.GetType());
+      Console.WriteLine(newItem.GetType());
     }
 
     [TestMethod]
