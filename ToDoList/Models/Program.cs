23790262006b9387ec2d43b4bb01349be4527c42
nkhakwan <post_khan@yahoo.com>
2020-05-12 17:25:18 -0700

    commiting after removal of extra code from Program.cs

--- ToDoList/Models/Program.cs
+++ ToDoList/Models/Program.cs
@@ -10,14 +10,14 @@ namespace DayOfTheWeekCalculator
     {
       Console.WriteLine("Please enter Date, month, year separated by a comma");
       string MyInput = Console.ReadLine();
-      string[] MyDate = MyInput.Split(',');
+      string[] MyDate = MyInput.Split(','); // List didn't work as it needs to be added.
       WeekDay MyWeekDay = new WeekDay (int.Parse(MyDate[0]), int.Parse(MyDate[1]), int.Parse(MyDate[2]));
       Console.WriteLine($"here are the number of days {MyWeekDay.NumberOfDays()}");
 
       List<string> myString = new List<string>();
       for (int i = 0; i<MyDate.Length; i=i+1)
       {
-        myString[i] = MyDate[i];
+        myString.Add(MyDate[i]);
         Console.WriteLine($"Here is the list items{myString[i]} ");
       }
       
@@ -26,3 +26,4 @@ namespace DayOfTheWeekCalculator
     }
   }
 }
+
